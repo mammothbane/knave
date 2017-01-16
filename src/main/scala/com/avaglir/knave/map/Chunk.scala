bd52d51b8aab5d5e87388a8b56a7b3b927fd3b79
Nathan Perry <avaglir@gmail.com>
2017-01-16 16:17:46 -0500

    start on chunk impl

---
+++ src/main/scala/com/avaglir/knave/map/Chunk.scala
@@ -0,0 +1,20 @@
+package com.avaglir.knave.map
+
+import com.avaglir.knave.util._
+
+class Chunk(seed: Float) {
+  import Chunk._
+
+  private val tiles = Array.ofDim[Tile](DIMENS, DIMENS)
+
+  def apply(v: Vector2) = {
+    if (v.x >= DIMENS || v.y >= DIMENS) throw new IllegalArgumentException
+    tiles(v.x)(v.y)
+  }
+
+
+}
+
+object Chunk {
+  val DIMENS = 64
+}
