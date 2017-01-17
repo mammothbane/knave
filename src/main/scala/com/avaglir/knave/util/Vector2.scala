ac4dbd374da7c3eb4074d7022641a1afa532823b
Nathan Perry <avaglir@gmail.com>
2017-01-17 14:30:37 -0500

    first pass converting Vector2 to use Numeric

--- src/main/scala/com/avaglir/knave/util/Vector2.scala
+++ src/main/scala/com/avaglir/knave/util/Vector2.scala
@@ -1,24 +1,34 @@
 package com.avaglir.knave.util
 
-case class Vector2(x: Int, y: Int) {
-  def +(other: Vector2) = Vector2(x + other.x, y + other.y)
-  def -(other: Vector2) = Vector2(x - other.x, y - other.y)
-  def dot(other: Vector2) = x * other.x + y * other.y
+case class Vector2[T: Numeric](x: T, y: T) {
+  private lazy val num = implicitly[Numeric[T]]
+  import num._
 
-  def unary_- = this.map(elem => -elem)
+  def +[V](other: Vector2[V])(implicit conv: V => T) = Vector2(x + other.x, y + other.y)
+  def -[V](other: Vector2[V])(implicit conv: V => T) = Vector2(x - other.x, y - other.y)
+  def dot[V](other: Vector2[V])(implicit conv: V => T) = x * other.x + y * other.y
 
-  def *(factor: Float) = map { (elem) => (elem * factor).toInt }
-  def /(factor: Float) = map { (elem) => (elem / factor).toInt }
+  def unary_-(): Vector2[T] = map(num.negate)
+
+  def *[V](factor: V)(implicit conv: V => T) = map { (elem) => num.times(elem, factor) }
+  def /[V](factor: V)(implicit conv: V => T) = num match {
+    case n: Integral[T] => map { (elem) => n.quot(elem, factor) }
+    case n: Fractional[T] => map { (elem) => n.div(elem, factor) }
+  }
 
   def half = this / 2
-  def magnitude: Float = math.sqrt(x*x + y*y).toFloat
+  def magnitude: Double = {
+    val doubleX = x.toDouble
+    val doubleY = y.toDouble
+    math.sqrt(doubleX*doubleX + doubleY*doubleY)
+  }
   def normalize = this / magnitude
   def transpose = Vector2(y, x)
 
-  def map(fn: (Int) => Int) = Vector2(fn(x), fn(y))
+  def map[V](fn: (V) => V)(implicit conv: V => T): Vector2[T] = Vector2(fn(x), fn(y))
 
-  def adjacent: Set[Vector2] = adjacent(false)
-  def adjacent(diag: Boolean): Set[Vector2] = Set(
+  def adjacent: Set[Vector2[T]] = adjacent(false)
+  def adjacent(diag: Boolean): Set[Vector2[T]] = Set(
     this + Vector2.UP,
     this + Vector2.DOWN,
     this + Vector2.LEFT,
@@ -30,7 +40,8 @@ case class Vector2(x: Int, y: Int) {
     this + Vector2.DOWN + Vector2.LEFT
   ) else Set.empty)
 
-  def componentsClamped(max: Vector2, min: Vector2 = Vector2.ZERO): Boolean = this >= min && this < max
+  def componentsClamped[V](max: Vector2[V])(implicit conv: V => T): Boolean = componentsClamped(max, Vector2.ZERO)
+  def componentsClamped[V, U](max: Vector2[V], min: Vector2[U])(implicit conv: V => T, convU: U => T): Boolean = this >= min && this < max
 
   override def equals(obj: scala.Any): Boolean = {
     if (!obj.isInstanceOf[Vector2]) return false
@@ -41,25 +52,25 @@ case class Vector2(x: Int, y: Int) {
 
   override def toString: String = s"v2($x, $y)"
 
-  def <=(other: Vector2): Boolean = this == other || this < other
-  def <(other: Vector2): Boolean = x < other.x && y < other.y
-  def >=(other: Vector2): Boolean = this == other || this > other
-  def >(other: Vector2): Boolean = x > other.y && y > other.y
+  def <=[V](other: Vector2[V])(implicit conv: V => T): Boolean = this == other || this < other
+  def <[V](other: Vector2[V])(implicit conv: V => T): Boolean = x < other.x && y < other.y
+  def >=[V](other: Vector2[V])(implicit conv: V => T): Boolean = this == other || this > other
+  def >[V](other: Vector2[V])(implicit conv: V => T): Boolean = x > other.y && y > other.y
 
   def octant: Int = {
-    if (x > 0 && y >= 0) { // first quadrant
+    if (x > zero && y >= zero) { // first quadrant
       if (x >= y) 1
       else 2
-    } else if (y > 0 && x <= 0) {  // second quadrant
+    } else if (y > zero && x <= zero) {  // second quadrant
       if (-x >= y) 4
       else 3
-    } else if (y <= 0 && x < 0) { // third quadrant
+    } else if (y <= zero && x < zero) { // third quadrant
       if (-x >= -y) 5
       else 6
-    } else if (y < 0 && x >= 0) { // fourth quadrant
+    } else if (y < zero && x >= zero) { // fourth quadrant
       if (x >= -y) 8
       else 7
-    } else 0
+    } else 0 // origin
   }
 }
 
