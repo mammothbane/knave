90799f550fc5c7f1f7cb0391605215173ac59b35
Nathan Perry <avaglir@gmail.com>
2017-01-22 20:06:19 -0500

    actually implement eviction for lru

--- src/main/scala/com/avaglir/knave/util/Lru.scala
+++ src/main/scala/com/avaglir/knave/util/Lru.scala
@@ -3,12 +3,17 @@ package com.avaglir.knave.util
 import scala.collection.mutable
 
 class Lru[K, V](val maxElems: Int, compute: K => V, val expiration: Option[Int], initial: K*) {
+  private var lastEvict = System.currentTimeMillis
+
   private val lru = mutable.Map[K, Long](initial.map { (_, System.currentTimeMillis ) }: _*)
   private val vals = mutable.Map(initial.map { elem => (elem, compute(elem)) }: _*)
 
   def apply(k: K): V = {
+    val now = System.currentTimeMillis
+    if (expiration.isDefined && now - lastEvict > expiration.get) evict()
+
     if (vals contains k) {
-      lru(k) = System.currentTimeMillis
+      lru(k) = now
       vals(k)
     } else {
       if (vals.size == maxElems) {
@@ -19,10 +24,23 @@ class Lru[K, V](val maxElems: Int, compute: K => V, val expiration: Option[Int],
 
       val upd = compute(k)
       vals(k) = upd
-      lru(k) = System.currentTimeMillis
+      lru(k) = now
       upd
     }
   }
 
-  def invalidate(k: K): Option[V] = vals remove k
+  def invalidate(k: K): Option[V] = {
+    lru remove k
+    vals remove k
+  }
+
+  private def evict(): Unit = {
+    val now = System.currentTimeMillis
+    lastEvict = now
+
+    lru.filter { case (_, time) => now - time > expiration.get }.unzip._1.foreach { key =>
+      lru remove key
+      vals remove key
+    }
+  }
 }
